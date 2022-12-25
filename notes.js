a1b9f32f121566d5956ffcea7b90056aae86528f
Itu <itumandal22@gmail.com>
2022-12-25 09:00:12 +0530

    added notes

---
+++ notes.js
@@ -0,0 +1,10 @@
+/*
+! 1. What is Emmet?
+?-> Emmet is a web-developer’s toolkit that can greatly improve HTML & CSS workflow.
+
+! 2. Difference between a Library and Framework?
+?-> 1. A library is a collection of functions developers can call at will and still control the flow of the software application. The operations performed to be performed by libraries are precisely and specifically defined.
+?   2. A framework is a structure in which the application defines the content of the operation by asking the developer to fill in the spaces. The framework defines the concept with the functionality itself is defined by the developer with the end-user in mind.
+?   3.Frameworks and libraries are code written by third parties to solve regular/common problems or to optimise performance. A key difference between the two is the inversion of control. When using a library, the control remains with the developer who tells the application when to call library functions. When using a framework, the control is reversed, which means that the framework tells the developer where code needs to be provided and calls it as it requires.
+
+*/
