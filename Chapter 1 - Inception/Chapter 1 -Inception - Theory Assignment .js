d5c895fed8661aeb3b671e9d828493c45767d4c3
Itu <itumandal22@gmail.com>
2023-01-25 16:49:22 +0530

    added Inception Ch 1

--- Chapter 1 - Inception/Chapter 1 -Inception - Theory Assignment .js
+++ Chapter 1 - Inception/Chapter 1 -Inception - Theory Assignment .js
@@ -9,6 +9,7 @@
 
 ! What is CDN? Why do we use it?
 ? A Content Delivery Network or a CDN is a globally-distributed network of servers that helps provide high-availability, faster performance, and security to websites distributing their content via it.
+? A content delivery network (CDN) refers to a geographically distributed group of servers which work together to provide fast delivery of Internet content. A CDN allows for the quick transfer of assets needed for loading Internet content including HTML pages, javascript files, stylesheets, images, and videos.
 ! Why do we use it?
 ? 1. Increasing Availability - For simplicity, availability can be considered a simple measure of how long your website and its functions remain accessible in a given period.With a CDN coming into the picture, it does two things. One, a lot of traffic doesn't even come to your servers. The edge server of the CDN serves a lot of content from its cache. So, you need a slightly fewer number of servers.
 ? Second, as long as the content is available in the CDNs cache, even if your actual servers are not working, the CDN will keep serving the content. This gives you some buffer time to fix issues on your servers while the CDN serves whatever content it can from its cache.
@@ -17,10 +18,22 @@
 
 ! ● Why is React known as React?
 ? React was developed for applications (Facebook) that have constantly changing data. Since React is a front-end framework or the “View” in MVC, this means that as the user clicks around and changes the app's data, the view should “react” or change with those user events.
-● What is crossorigin in script tag?
-● What is diference between React and ReactDOM
-● What is difference between react.development.js and react.production.js files via CDN?
-● What is async and defer? - see my Youtube video ;)
+
+! ● What is crossorigin in script tag?
+? The crossorigin attribute sets the mode of the request to an HTTP CORS Request. Web pages often make requests to load resources on other servers. Here is where CORS comes in. A cross-origin request is a request for a resource (e.g. style sheets, iframes, images, fonts, or scripts) from another domain.
+? CORS stands for Cross-Origin Resource Sharing, and is a mechanism that allows resources on a web page to be requested from another domain outside their own domain. It defines a way of how a browser and server can interact to determine whether it is safe to allow the cross-origin request. CORS allows servers to specify who can access the assets on the server, among many other things.
+
+! ● What is diference between React and ReactDOM ?
+? React is a JavaScript library for building User Interfaces whereas ReactDOM is also JavaScript library that allows React to interact with the DOM. The react package contains React.createElement(), React.Component, React.Children, and other helpers related to elements and component classes. You can think of these as the isomorphic or universal helpers that you need to build components. The react-dom package contains ReactDOM.render(), and in react-dom/server we have server-side rendering support with ReactDOMServer.renderToString() and ReactDOMServer.renderToStaticMarkup().
+
+! ● What is difference between react.development.js and react.production.js files via CDN ?
+? Development is the stage of an application before it's made public while production is the term used for the same application when it's made public.
+
+! ● What is async and defer? 
+? async and defer both load JavaScript asynchronously without render blocking, but async executes as soon as possible while defer runs in sequence toward the end of the loading process, just before the DOMContentLoaded event.
+? Any given resource on a page is first downloaded and then applied to or run on the page. While the loading behavior of async and defer are the same - loading in the background without render blocking - the difference lies in their behavior when executing.
+? JavaScript files with the async attribute will run in no particular order as soon as they're loaded, whereas defer'd resources run in sequence (in the order they appear in the HTML) toward the end of the initial loading process, right before the DOMContentLoaded event.
+
 Coding -
 ● Set up all the tools in your laptop
 ○ VS Code
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Namaste_React
+## Namaste_React by Akshay Saini.
 
-React JS code and notes
+Here you will find all React JS code and notes.
+
+# Chapter 1 - Inception (Date: 24-12-2022)
+
+- Theory Assignment
+- Coding Assignment
+
+  # Chapter 02 - Igniting our App (Date: 31-12-2022)
+
+  # Chapter 03 - Laying the Foundation (Date: 01-01-2023)
